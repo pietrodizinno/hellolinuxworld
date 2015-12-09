6b19b1b8c5a9f4470c75c508878440905e4de34b
Chris Smart <csmart@kororaproject.org>
2015-12-09 15:15:52 +1100

    formatting fixes in line with kernel style guide

--- src/hello.c
+++ src/hello.c
@@ -1,16 +1,20 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#define pr_fmt(fmt)	"distroguy: "	fmt
+
 MODULE_AUTHOR("Chris Smart <distroguy@gmail.com>");
-MODULE_DESCRIPTION("Print hello world");
+MODULE_DESCRIPTION("Print Hello World");
 MODULE_LICENSE("GPL");
 
-int init_module() {
-  printk("Hello, Linux World!\n");
-  return 0;
+int init_module(void)
+{
+	pr_info("Hello, Linux World!\n");
+	return 0;
 }
 
-void cleanup_module() {
-  printk("Bye bye, Linux World!\n");
+void cleanup_module(void)
+{
+	pr_info("Bye bye, Linux World!\n");
 }
 
